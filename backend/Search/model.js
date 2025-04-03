d1d869b15d641790a0f83cb277837e55a80f93c8
bosspaoofGNGBarbershop <johnpaolo.creus@g.batstate-u.edu.ph>
2025-04-03 13:01:12 +0800

    paolo

--- backend/Search/model.js
+++ backend/Search/model.js
@@ -1,14 +1,9 @@
-class Course {
-    static courses = [
-        { course_name: "Computer Science 101", description: "Introduction to Computer Science", credits: 3 },
-        { course_name: "Mathematics 201", description: "Advanced Calculus and Algebra", credits: 4 },
-        { course_name: "Physics 101", description: "Fundamentals of Physics", credits: 3 },
-        { course_name: "Business Management 301", description: "Principles of Business Management", credits: 3 }
-    ];
+const mongoose = require("mongoose");
 
-    static findByName(courseName) {
-        return this.courses.find(c => c.course_name.toLowerCase() === courseName);
-    }
-}
+const CourseSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    credits: Number,
+});
 
-module.exports = Course;
+module.exports = mongoose.model("Course", CourseSchema);
--- backend/Search/server.js
+++ backend/Search/server.js
@@ -5,6 +5,11 @@ const courseRouter = require('./router');
 
 app.use('/course', courseRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connect(process.env.DB)
+    .then(() =>{
+        app.listen(process.env.PORT, () =>{
+            console.log(`Connected to database and listening to port ${process.env.PORT}`)
+        })
+    }).catch(error =>{
+        console.log(error)
+    })
