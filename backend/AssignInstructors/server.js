1dc185f51d39813f8a80dcff3ac5bddeb1eea72d
Franz <franzciriaco1@gmail.com>
2025-04-03 11:10:00 +0800

    franz

--- backend/AssignInstructors/server.js
+++ backend/AssignInstructors/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const myRouter = require('./router')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const courseRoutes = require('./router'); 
 
-const app = express()
 
-app.use(express.json())
-mongoose.connect('kunyare merong url')
-.then(() =>{
-    app.listen(3000, () => {
-        console.log('Listen to port 3000')
-    })
-})
-.catch(error =>{
-    console.log(error)
-})
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(myRouter)
+app.use('/courses', courseRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
